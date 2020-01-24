70354d5f437b5fa385c2753fb420d3f42e06c680
Artem Shilin <shilinartem@gmail.com>
2020-01-24 21:33:41 +0300

    fix logic

--- THPinViewController/THPinView.m
+++ THPinViewController/THPinView.m
@@ -253,7 +253,8 @@
     [self.input appendString:[NSString stringWithFormat:@"%lu", (unsigned long)number]];
     [self.inputCirclesView fillCircleAtPosition:self.input.length - 1];
 
-    [self.delegate pinView:self isPinValid:self.input];
+    [self.delegate pinView:self didAddNumberToCurrentPin:self.input];
+
     
     [self updateBottomButton];
     
